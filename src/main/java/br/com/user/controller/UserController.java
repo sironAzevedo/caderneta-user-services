df68f253c1abf743d8c2237457805f08a6f46d14
sironAzevedo <sirondba@gmail.com>
2025-04-20 04:05:53 -0300

    Ajuste na validação de token e endpoint de login

--- src/main/java/br/com/user/controller/UserController.java
+++ src/main/java/br/com/user/controller/UserController.java
@@ -8,7 +8,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -28,11 +35,12 @@ public class UserController {
 		service.create(dto);
 	}
 
-	@MethodLoggable
 	@ResponseBody
-	@GetMapping("/login")
+	@ValidationUser
+	@MethodLoggable
+	@GetMapping("/login/{email}")
 	@ResponseStatus(value = HttpStatus.OK)
-	public UserDTO login(@RequestParam(value = "email") String email) {
+	public UserDTO login(@PathVariable("email") String email) {
 		return service.login(email);
 	}
 
@@ -48,6 +56,7 @@ public class UserController {
 
 	@GetMapping
 	@ResponseBody
+	@ValidationUser
 	@MethodLoggable
 	@ResponseStatus(value = HttpStatus.OK)
 	@Operation(summary = "Find All User")
